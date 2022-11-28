4ce293feb57b5fb4b095594b4f825d520b704ffe
shariq512000 <www.shariq512000@gmail.com>
2022-11-28 06:20:55 -0800

    repush

--- server.mjs
+++ server.mjs
@@ -1,13 +1,18 @@
-console.log("hello world")
-
+import path from 'path';
 import express from 'express';
+
+const __dirname = path.resolve()
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
-    console.log("request ip:", req.ip)
-  res.send('Hello World!' + new Date().toString())
-})
+// app.get('/', (req, res) => {
+//     console.log("request ip:", req.ip)
+//   res.send('Hello World!' + new Date().toString())
+// })
+
+app.use('/', express.static(path.join(__dirname, './web/build')))
+app.use('*', express.static(path.join(__dirname, './web/build')))
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
---
+++ web/src/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div>
+      hello world static host on express server
+    </div>
+  );
+}
+
+export default App;
